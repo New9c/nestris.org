041ff65cbf3d9a935f8cc9316f09934c3cd01105
Ansel Chang <ansel.chang@yahoo.com>
2025-03-22 01:45:25 -0400

    clicking username in ranked should open player profile

--- client/src/app/components/layout/room/game-header/game-header.component.ts
+++ client/src/app/components/layout/room/game-header/game-header.component.ts
@@ -1,4 +1,6 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ProfileModalConfig } from 'src/app/components/modals/profile-modal/profile-modal.component';
+import { ModalManagerService, ModalType } from 'src/app/services/modal-manager.service';
 
 @Component({
   selector: 'app-game-header',
@@ -9,8 +11,21 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 export class GameHeaderComponent {
   @Input() trophies: number = 0;
   @Input() username: string = '';
+  @Input() userid?: string;
   @Input() score: number = 0;
   @Input() color: 'red' | 'blue' = 'red';
   @Input() rated: boolean = false;
+
+  constructor(
+    private readonly modalManagerService: ModalManagerService 
+  ) {}
+
+  openProfile(event: MouseEvent) {
+    event.stopPropagation();
+    if (this.userid) {
+      const config: ProfileModalConfig = { userid: this.userid };
+      this.modalManagerService.showModal(ModalType.PROFILE, config);
+    }
+  }
 
 }
