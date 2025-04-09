7c6d9fdf06f0cfa9dd63ece39e48c933ccbc37a5
9c <ericgwhuang@gmail.com>
2025-04-09 08:42:28 +0800

    fix: New match (hopefully) will activate via gamepad

--- client/src/app/components/layout/room/modals/multiplayer-after-match-modal/multiplayer-after-match-modal.component.ts
+++ client/src/app/components/layout/room/modals/multiplayer-after-match-modal/multiplayer-after-match-modal.component.ts
@@ -1,10 +1,11 @@
-import { ChangeDetectionStrategy, Component, HostListener } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostListener, OnDestroy } from '@angular/core';
 import { RoomService } from 'src/app/services/room/room.service';
 import { calculateScoreForPlayer, MultiplayerRoomState, MultiplayerRoomStatus, PlayerIndex, pointWinner } from 'src/app/shared/room/multiplayer-room-models';
 import { MultiplayerComponent } from '../../multiplayer-room/multiplayer-component';
 import { ButtonColor } from 'src/app/components/ui/solid-button/solid-button.component';
 import { Router } from '@angular/router';
 import { RankedQueueService } from 'src/app/services/room/ranked-queue.service';
+import { GamepadService } from 'src/app/services/gamepad.service';
 import { MeService } from 'src/app/services/state/me.service';
 
 @Component({
@@ -13,15 +14,18 @@ import { MeService } from 'src/app/services/state/me.service';
   styleUrls: ['./multiplayer-after-match-modal.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MultiplayerAfterMatchModalComponent extends MultiplayerComponent {
+export class MultiplayerAfterMatchModalComponent extends MultiplayerComponent implements OnDestroy {
 
+  private gamepadSubscription: any;
   constructor(
     roomService: RoomService,
     private readonly queueService: RankedQueueService,
     private readonly meService: MeService,
+    private readonly gamepadService: GamepadService,
     private readonly router: Router,
   ) {
     super(roomService);
+    this.gamepadSubscription = this.gamepadService.onPress().subscribe(key => this.onKeyDown(key));
   }
 
   readonly calculateScoreForPlayer = calculateScoreForPlayer;
@@ -29,6 +33,10 @@ export class MultiplayerAfterMatchModalComponent extends MultiplayerComponent {
   readonly ButtonColor = ButtonColor;
   readonly MultiplayerRoomStatus = MultiplayerRoomStatus;
 
+
+  ngOnDestroy() {
+    this.gamepadSubscription.unsubscribe();
+  }
 
   getPointText(index: PlayerIndex): string {
     if (index === PlayerIndex.DRAW) return 'Draw';
@@ -50,17 +58,17 @@ export class MultiplayerAfterMatchModalComponent extends MultiplayerComponent {
 
     if (state.ranked) {
       // Leave the room
-    await this.roomService.leaveRoom();
+      await this.roomService.leaveRoom();
 
-    // Join the queue
-    await this.queueService.joinQueue();
+      // Join the queue
+      await this.queueService.joinQueue();
 
     } else {
       // Send rematch offer
       this.multiplayerClientRoom.sendReadyEvent();
     }
 
-    
+
   }
 
   async exit() {
@@ -71,10 +79,14 @@ export class MultiplayerAfterMatchModalComponent extends MultiplayerComponent {
   // Pressing "space" or "enter" should also trigger the next button
   @HostListener('document:keydown', ['$event'])
   handleKeyboardEvent(event: KeyboardEvent) {
+    this.onKeyDown(event.key);
+  }
+
+  private onKeyDown(key: string) {
     const me = this.meService.getSync();
     if (!me) return;
 
-    if (event.key === me.keybind_emu_start) {
+    if (key === me.keybind_emu_start) {
       this.playNewMatch(this.multiplayerClientRoom.getState<MultiplayerRoomState>());
     }
   }
